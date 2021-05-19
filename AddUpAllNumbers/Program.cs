4fa9a7928b96cd4becd66d8cfaa97308eceb474d
Mattea Swain <mmswain@tql.com>
2021-05-19 12:18:26 -0400

    Add project files.

---
+++ AddUpAllNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddUpAllNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Total = 0;
+            
+            for (var number = 1; number <= 50; number++) 
+            {
+                if (number % 5 == 0 || number % 7 == 0)
+                {
+                    Total = Total + number;
+
+                    
+
+                } 
+            }
+
+            Console.Write($"Total is {Total} ");
+        }
+
+    }
+}
